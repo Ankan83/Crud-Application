37ce48e372c12bed8bd4d8b7883ffcb778eee78f
Ankan83 <ankan.rastogi30@gmail.com>
2025-03-04 02:03:26 +0530

    Remove unused buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,44 +31,10 @@ export default function RootLayout({
       >
         <QueryProvider>
           <div className="navbar bg-base-100">
-            <div className="flex-none">
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block h-5 w-5 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </button>
-            </div>
             <div className="flex-1">
               <Link href="/post" className="btn btn-ghost text-xl">
                 Posts
               </Link>
-            </div>
-            <div className="flex-none">
-              <button className="btn btn-square btn-ghost">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block h-5 w-5 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                  ></path>
-                </svg>
-              </button>
             </div>
           </div>
           <div className="flex w-full">
